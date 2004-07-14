1e152083ccbc58c14b3b6cb13255c88f806d63aa
rncbc <rncbc@6e21f190-4f5b-40a6-a6af-ed3d11dd9ba7>
2004-07-14 15:02:49 +0000

    Output meters are now an optional feature (fix).
    git-svn-id: http://svn.code.sf.net/p/qsynth/code_attic/trunk@122 6e21f190-4f5b-40a6-a6af-ed3d11dd9ba7

--- src/qsynthAbout.h
+++ src/qsynthAbout.h
@@ -24,7 +24,7 @@
 
 #define QSYNTH_TITLE        "qsynth"
 #define QSYNTH_SUBTITLE	    "A fluidsynth Qt GUI Interface"
-#define QSYNTH_VERSION      "0.2.1.97"
+#define QSYNTH_VERSION      "0.2.1.98"
 #define QSYNTH_WEBSITE      "http://qsynth.sourceforge.net"
 #define QSYNTH_COPYRIGHT    "Copyright (C) 2003-2004, rncbc aka Rui Nuno Capela. All rights reserved."
 
