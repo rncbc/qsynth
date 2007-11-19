0f0f2569923eff26f00e6f0d4d5c94873f6784be
rncbc <rncbc@6e21f190-4f5b-40a6-a6af-ed3d11dd9ba7>
2007-11-19 09:17:32 +0000

    - Updated win32/config.h for mingw32 and newer fluidsynth 1.0.8.
    git-svn-id: http://svn.code.sf.net/p/qsynth/code_attic/trunk@239 6e21f190-4f5b-40a6-a6af-ed3d11dd9ba7

--- win32/config.h
+++ win32/config.h
@@ -1,15 +1,15 @@
 
 #define PACKAGE_NAME    "Qsynth"
-#define PACKAGE_VERSION "0.3.1.11"
-
+#define PACKAGE_VERSION "0.3.1.12"
 
 #define CONFIG_PREFIX   "."
 #define CONFIG_DEBUG    1
 
 #define CONFIG_SYSTEM_TRAY
 
+#if !defined(__MINGW32__)
 #undef  CONFIG_FLUID_SERVER
 #undef  CONFIG_FLUID_RESET
 #undef  CONFIG_FLUID_BANK_OFFSET
-
 #undef  CONFIG_ROUND
+#endif
