0523502be085709724f76778e853681259de4b5c
rncbc <rncbc@rncbc.org>
2017-01-02 10:31:57 +0000

    - Update to brand New Hear (2017).

--- src/qsynthAbout.h
+++ src/qsynthAbout.h
@@ -1,7 +1,7 @@
 // qsynthAbout.h
 //
 /****************************************************************************
-   Copyright (C) 2003-2016, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2003-2017, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -28,10 +28,11 @@
 
 #define QSYNTH_SUBTITLE     "A fluidsynth Qt GUI Interface"
 #define QSYNTH_WEBSITE      "http://qsynth.sourceforge.net"
-#define QSYNTH_COPYRIGHT    "Copyright (C) 2003-2016, rncbc aka Rui Nuno Capela. All rights reserved."
+#define QSYNTH_COPYRIGHT    "Copyright (C) 2003-2017, rncbc aka Rui Nuno Capela. All rights reserved."
 
 #define QSYNTH_DOMAIN       "rncbc.org"
 
 #endif  // __qsynthAbout_h
 
 // end of qsynthAbout.h
+
