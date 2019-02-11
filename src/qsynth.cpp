1870a999a0fe605ef84d1ab760c55b74652a5701
rncbc <rncbc@rncbc.org>
2019-02-11 09:24:31 +0000

    - HiDPI display screen support (Qt >= 5.6). (EXPERIMENTAL)

--- src/qsynth.cpp
+++ src/qsynth.cpp
@@ -1,7 +1,7 @@
 // qsynth.cpp
 //
 /****************************************************************************
-   Copyright (C) 2003-2018, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2003-2019, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -423,7 +423,9 @@ int main ( int argc, char **argv )
 #endif
 #endif
 	qsynthApplication app(argc, argv);
-
+#if QT_VERSION >= 0x050600
+	app.setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 	// Construct default settings; override with command line arguments.
 	qsynthOptions settings;
 	if (!settings.parse_args(app.arguments())) {
