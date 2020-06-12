ecc3cd42cd27f97942abca63bac5aa9598cb5302
rncbc <rncbc@rncbc.org>
2020-06-13 00:58:11 +0100

    - Left-clicking on the system-tray icon toggles the main   widget visibility, disregarding if already undercover   to other windows. (FIXED)

--- src/qsynthMainForm.cpp
+++ src/qsynthMainForm.cpp
@@ -1509,7 +1509,7 @@ void qsynthMainForm::toggleMainForm (void)
 
 	m_pOptions->saveWidgetGeometry(this, true);
 
-	if (isVisible() && !isMinimized()) {
+	if (isVisible() && !isMinimized() && isActiveWindow()) {
 	#ifdef CONFIG_SYSTEM_TRAY
 		// Hide away from sight, totally...
 		if (m_pOptions->bSystemTray && m_pSystemTray)
