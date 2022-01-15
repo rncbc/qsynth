81dad27730f4052b0613e56fef090458d5effe4a
rncbc <rncbc@rncbc.org>
2022-01-15 15:40:27 +0000

    - Fixed translations path to be relative to application runtime.

--- src/qsynth.cpp
+++ src/qsynth.cpp
@@ -1,7 +1,7 @@
 // qsynth.cpp
 //
 /****************************************************************************
-   Copyright (C) 2003-2021, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2003-2022, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -44,6 +44,10 @@ const WindowFlags WindowCloseButtonHint = WindowFlags(0x08000000);
 
 #ifndef CONFIG_PREFIX
 #define CONFIG_PREFIX	"/usr/local"
+#endif
+
+#ifndef CONFIG_BINDIR
+#define CONFIG_BINDIR	CONFIG_PREFIX "/bin"
 #endif
 
 #ifndef CONFIG_DATADIR
@@ -133,7 +137,9 @@ qsynthApplication::qsynthApplication ( int& argc, char **argv )
 		if (m_pMyTranslator->load(sLocName, sLocPath)) {
 			QApplication::installTranslator(m_pMyTranslator);
 		} else {
-			sLocPath = CONFIG_DATADIR "/qsynth/translations";
+			sLocPath = QApplication::applicationDirPath();
+			sLocPath.remove(CONFIG_BINDIR);
+			sLocPath.append(CONFIG_DATADIR "/qsynth/translations");
 			if (m_pMyTranslator->load(sLocName, sLocPath)) {
 				QApplication::installTranslator(m_pMyTranslator);
 			} else {
