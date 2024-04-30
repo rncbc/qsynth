b728bf62383c13b5342cfb96a9cc8e5ee5a6764d
rncbc <rncbc@rncbc.org>
2024-04-30 11:26:01 +0100

    Revert "another place that needs to be utf8 encoded"
    This reverts commit 12b5e5465aff1406d1becc7c9c5d6048fba6f93d.

--- src/qsynthSetup.cpp
+++ src/qsynthSetup.cpp
@@ -122,8 +122,8 @@ void qsynthSetup::realize (void)
 		else
 			sAudioKey += "device";
 		::fluid_settings_setstr(m_pFluidSettings,
-			sAudioKey.toUtf8().data(),
-			sAudioDevice.toUtf8().data());
+			sAudioKey.toLocal8Bit().data(),
+			sAudioDevice.toLocal8Bit().data());
 	}
 	if (!sJackName.isEmpty()) {
 		pszKey = (char *) "audio.jack.id";
